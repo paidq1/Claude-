0d9876270c20d18e9691e8e339641dbf22fc5478
paidq1 <primevextpro@gmail.com>
2026-04-07 14:49:16 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div style={{ padding: 20 }}>
       <h1>App is running 🚀</h1>
-      <p>Deployment fixed successfully</p>
+      <p>Build fixed successfully</p>
     </div>
   );
 }
